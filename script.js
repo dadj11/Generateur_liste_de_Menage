f6410a3c125835f01c3503486208ba901f8c71a2
dadj11 <godwindadja@gmail.com>
2026-03-31 13:22:18 +0100

    auto niveau

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+
+
 var Etudient =[]
-var Niveaux=[]
+var Niveaux=['L1','L2','L3']
 var Zone_de_menage=[]
 
 
+var select= document.getElementById('niveau');
+
+Niveaux.forEach((element)=>{
+  var option=document.createElement('option')
+  option.textContent = element
+  option.value = element
+  select.appendChild(option);
+})
+
+
+function generatePDF() {
+  let pdf = new jsPDF("p", "pt", "a4");
+  let options = { pagesplit : true };
+  pdf.addHTML($("#body"), options, () => {
+    pdf.save("monDocument.pdf");
+  });
+}   
 
 
 
